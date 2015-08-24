4d76f856fb680c2a8c611a5d7c85b9bc12d47c00
dodoliu <liudongyue1987@163.com>
2015-08-24 18:50:53 +0800

    update excel_sheet_column_number.rb

--- excel_sheet_column_number.rb
+++ excel_sheet_column_number.rb
@@ -1,12 +1,11 @@
 # @param {String} s
 # @return {Integer}
 def title_to_number(s)
-    letters = []
-    char_array = []
-    A..Z.each_with_index { |char, index| char_array.push char => index + 1  }
-    
+    hash_chars = Hash.new
+    ("A".."Z").each_with_index { |char, index| hash_chars[char] = index + 1 }
     result = 0;
     s.each_char do |char|
-        result += char_array[char]
+        result += hash_chars[char]
     end
+    return result
 end
